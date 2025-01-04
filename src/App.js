98ad4ed42518eede4149ebcadc990fe84b92ee49
PhuNguyen25 <phu159123@gmail.com>
2025-01-04 12:15:17 +0700

    update

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {
         </div>
       ) : (
         <FacebookLogin
-          appId="1047240919599155"
+          appId="1052016440060467"
           autoLoad={false}
           fields="name,email,picture"
           callback={responseFacebook}
